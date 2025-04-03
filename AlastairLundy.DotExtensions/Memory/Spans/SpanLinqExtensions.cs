27367ebcc4eafdb5dee1c7c0e79ff1a02ccb3e74
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-03 22:39:26 +0100

    add initial Span Linq extensions code

---
+++ AlastairLundy.DotExtensions/Memory/Spans/SpanLinqExtensions.cs
@@ -0,0 +1,199 @@
+﻿/*
+        MIT License
+
+       Copyright (c) 2025 Alastair Lundy
+
+       Permission is hereby granted, free of charge, to any person obtaining a copy
+       of this software and associated documentation files (the "Software"), to deal
+       in the Software without restriction, including without limitation the rights
+       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+       copies of the Software, and to permit persons to whom the Software is
+       furnished to do so, subject to the following conditions:
+
+       The above copyright notice and this permission notice shall be included in all
+       copies or substantial portions of the Software.
+
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+       SOFTWARE.
+   */
+
+using System;
+using System.Collections.Generic;
+
+namespace AlastairLundy.DotExtensions.Memory.Spans
+{
+    public static class SpanLinqExtensions
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Span<T> Except<T>(this Span<T> first, Span<T> second) where T : IEquatable<T>?
+        {
+            List<T> list = new();
+        
+            foreach (T item in first)
+            {
+                if (second.Contains(item) == false)
+                {
+                    list.Add(item);
+                }
+            }
+
+            foreach (T item in second)
+            {
+                if (first.Contains(item) == false)
+                {
+                    list.Add(item);
+                }
+            }
+        
+            return new Span<T>(list.ToArray());
+        }
+    
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="span"></param>
+        /// <param name="count"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Span<T> Skip<T>(this Span<T> span, int count)
+        {
+            List<T> list = new();
+        
+            for (int i = 0; i < span.Length; i++)
+            {
+                if ((i <= count) == false)
+                {
+                    list.Add(span[i]);
+                }
+            }
+        
+            return new Span<T>(list.ToArray());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="span"></param>
+        /// <param name="count"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Span<T> SkipLast<T>(this Span<T> span, int count)
+        {
+            List<T> list = new();
+
+            int limit = span.Length - count;
+
+            for (int i = 0; i < limit; i++)
+            {
+                list.Add(span[i]);
+            }
+
+            return new Span<T>(list.ToArray());
+        }
+    
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="span"></param>
+        /// <param name="predicate"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Span<T> SkipWhile<T>(this Span<T> span, Func<T, bool> predicate)
+        {
+            List<T> list = new();
+        
+            foreach (T item in span)
+            {
+                if (predicate.Invoke(item) == false)
+                {
+                    list.Add(item);
+                }
+            }
+        
+            return new Span<T>(list.ToArray());
+        }
+    
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="span"></param>
+        /// <param name="predicate"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static Span<T> Where<T>(this Span<T> span, Func<T, bool> predicate)
+        {
+            List<T> list = new();
+
+            foreach (T item in span)
+            {
+                if (predicate.Invoke(item))
+                {
+                    list.Add(item);
+                }
+            }
+        
+            return new Span<T>(list.ToArray());
+        } 
+    
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="span"></param>
+        /// <param name="predicate"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static bool Any<T>(this Span<T> span, Func<T, bool> predicate)
+        {
+            foreach (T item in span)
+            {
+                bool result = predicate.Invoke(item);
+
+                if (result)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="span"></param>
+        /// <param name="predicate"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static bool All<T>(this Span<T> span, Func<T, bool> predicate)
+        {
+            List<bool> results = new();
+
+            foreach (T item in span)
+            {
+                results.Add(predicate.Invoke(item));
+            }
+
+            foreach (bool result in results)
+            {
+                if (result == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
