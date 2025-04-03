b26cea7e5d5deee2692fa3ab00b87db2f632cd25
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-03 22:39:45 +0100

    Create SegmentContainsExtensions.cs

---
+++ AlastairLundy.DotExtensions.MsExtensions/System/StringSegments/SegmentContainsExtensions.cs
@@ -0,0 +1,119 @@
+﻿/*
+        MIT License
+
+       Copyright (c) 2025 Alastair Lundy
+
+       Permission is hereby granted, free of charge, to any person obtaining a copy
+       of this software and associated documentation files (the "Software"), to deal
+       in the Software without restriction, including without limitation the rights
+       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+       copies of the Software, and to permit persons to whom the Software is
+       furnished to do so, subject to the following conditions:
+
+       The above copyright notice and this permission notice shall be included in all
+       copies or substantial portions of the Software.
+
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+       SOFTWARE.
+   */
+
+using System.Linq;
+
+using Microsoft.Extensions.Primitives;
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace AlastairLundy.DotExtensions.MsExtensions.System.StringSegments;
+
+public static class SegmentContainsExtensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="this"></param>
+    /// <param name="character"></param>
+    /// <returns></returns>
+    public static bool Contains(this StringSegment @this, char character)
+    {
+        for (int i = 0; i < @this.Length; i++)
+        {
+            if (@this[i] == character)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="this"></param>
+    /// <param name="segment"></param>
+    /// <returns></returns>
+    public static bool Contains(this StringSegment @this, StringSegment segment)
+    {
+        if (@this.Length == segment.Length)
+        {
+            return @this.Equals(segment);
+        }
+        if (segment.Length > @this.Length)
+        {
+            return false;
+        }
+        
+        segment.
+
+        char[] segmentArray = segment.ToCharArray();
+
+        bool containsAllChars = segmentArray.All(x => @this.Contains(x));
+        
+        if (containsAllChars == false)
+        {
+            return false;
+        }
+        
+        int startIndex = 0;
+
+        for (int originIndex = 0; originIndex < @this.Length; originIndex++)
+        {
+            char c = @this[originIndex];
+            bool found = false;
+
+            for (int i = startIndex; i < segment.Length; i++)
+            {
+                if (segmentArray[i] == c)
+                {
+                    found = true;
+                    startIndex = i + 1;
+                    break;
+                }
+            }
+
+            if (found == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="this"></param>
+    /// <param name="str"></param>
+    /// <returns></returns>
+    public static bool Contains(this StringSegment @this, string str)
+    {
+        StringSegment val = new StringSegment(str);
+
+        return Contains(@this, val);
+    }
+}
