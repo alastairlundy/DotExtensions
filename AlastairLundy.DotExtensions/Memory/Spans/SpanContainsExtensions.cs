1704c12ededec5b0e0b4ba92a010ba3045c59358
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-04 12:02:17 +0100

    add Span Contains polyfill extension method

---
+++ AlastairLundy.DotExtensions/Memory/Spans/SpanContainsExtensions.cs
@@ -0,0 +1,54 @@
+﻿/*
+        MIT License
+       
+       Copyright (c) 2024-2025 Alastair Lundy
+       
+       Permission is hereby granted, free of charge, to any person obtaining a copy
+       of this software and associated documentation files (the "Software"), to deal
+       in the Software without restriction, including without limitation the rights
+       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+       copies of the Software, and to permit persons to whom the Software is
+       furnished to do so, subject to the following conditions:
+       
+       The above copyright notice and this permission notice shall be included in all
+       copies or substantial portions of the Software.
+       
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+       SOFTWARE.
+   */
+
+using System;
+
+namespace AlastairLundy.DotExtensions.Memory.Spans;
+
+public static class SpanContainsExtensions
+{
+    #if NETSTANDARD2_0 || NETSTANDARD2_1
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="span">The span to check through.</param>
+    /// <param name="item"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static bool Contains<T>(this Span<T> span, T item)
+    {
+        foreach (T t in span)
+        {
+            if (t is not null && t.Equals(item))
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    #endif
+}
