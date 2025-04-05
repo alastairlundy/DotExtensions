535b2ab7fb72415e5ef6dd7fb9d9e612d5dd624b
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-05 13:26:48 +0100

    add enumerable split extension methods

---
+++ AlastairLundy.DotExtensions/Collections/Generic/Enumerables/EnumerableSplitExtensions.cs
@@ -0,0 +1,98 @@
+﻿/*
+        MIT License
+       
+       Copyright (c) 2024-2025 Alastair Lundy
+       
+       Permission is hereby granted, free of charge, to any person obtaining a copy
+       of this software and associated documentation files (the "Software"), to deal
+       in the Software without restriction, including without limitation the rights
+       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+       copies of the Software, and to permit persons to whom the Software is
+       furnished to do so, subject to the following conditions:
+       
+       The above copyright notice and this permission notice shall be included in all
+       copies or substantial portions of the Software.
+       
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+       SOFTWARE.
+   */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlastairLundy.DotExtensions.Collections.Generic.Enumerables;
+
+public static class EnumerableSplitExtensions
+{
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="enumerable"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static IEnumerable<IEnumerable<T>> SplitByThreadCount<T>(this IEnumerable<T> enumerable)
+    {
+        T[] items = enumerable as T[] ?? enumerable.ToArray();
+
+        if (items.Length == 0)
+        {
+            throw new ArgumentException("Cannot split an empty enumerable.");
+        }
+        
+        double itemsPerThread = items.Length / Convert.ToDouble(Environment.ProcessorCount);
+        
+        int enumerableLimit = Convert.ToInt32(Math.Round(itemsPerThread, MidpointRounding.AwayFromZero));
+
+        return SplitByCount(items, enumerableLimit);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="enumerable"></param>
+    /// <param name="maxCount"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static IEnumerable<IEnumerable<T>> SplitByCount<T>(this IEnumerable<T> enumerable, int maxCount)
+    {
+        List<List<T>> outputList = new List<List<T>>();
+        
+        T[] items = enumerable as T[] ?? enumerable.ToArray();
+
+        if (items.Length == 0)
+        {
+            throw new ArgumentException("Cannot split an empty enumerable.");
+        }
+
+        int enumerableLimit = maxCount;
+        
+        int currentEnumerable = 0;
+
+        List<T> currentList = new List<T>();
+        
+        foreach (T item in items)
+        {
+            if (currentEnumerable < enumerableLimit)
+            {
+                currentList.Add(item);
+            }
+            else if (currentEnumerable == enumerableLimit)
+            {
+                outputList.Add(currentList);
+                currentList.Clear();
+                currentEnumerable = 0;
+            }
+        }
+
+        return outputList;
+    }
+}
