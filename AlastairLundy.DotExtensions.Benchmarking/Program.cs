57cf3fc7119060792026e01d7b5bfc8f3503525a
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-04-28 19:16:35 +0100

    add benchmarking code

---
+++ AlastairLundy.DotExtensions.Benchmarking/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using AlastairLundy.DotExtensions.Benchmarking.Benchmarks.Collections.ICollections;
+using BenchmarkDotNet.Running;
+
+Console.WriteLine("Hello World!");
+
+BenchmarkRunner.Run<EnumerableIndexExtensionsBenchmarks>();
