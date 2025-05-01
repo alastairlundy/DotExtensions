e41a46942a91e39f162658ff14b5dc3b2c0e23f5
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-01 18:32:13 +0200

    Update Program.cs

--- AlastairLundy.DotExtensions.Benchmarking/Program.cs
+++ AlastairLundy.DotExtensions.Benchmarking/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
-using AlastairLundy.DotExtensions.Benchmarking.Benchmarks.Collections.ICollections;
+using AlastairLundy.DotExtensions.Benchmarking.Benchmarks.Collections;
 using BenchmarkDotNet.Running;
 
 Console.WriteLine("Hello World!");
