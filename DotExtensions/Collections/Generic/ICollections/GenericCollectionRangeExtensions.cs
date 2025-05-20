1eb97c1ca7b265b07debdf7b5e590ca9be1df23b
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-20 11:29:11 +0100

    Update GenericCollectionRangeExtensions.cs

--- DotExtensions/Collections/Generic/ICollections/GenericCollectionRangeExtensions.cs
+++ DotExtensions/Collections/Generic/ICollections/GenericCollectionRangeExtensions.cs
@@ -24,7 +24,7 @@
 
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
 using AlastairLundy.DotExtensions.Collections.ILists;
 using AlastairLundy.DotExtensions.Localizations;
 
@@ -33,6 +33,31 @@ namespace AlastairLundy.DotExtensions.Collections.Generic.ICollections
     public static class GenericCollectionRangeExtensions
     {
 
+        /// <summary>
+        /// Appends elements from another collection to the end of the specified collection.
+        /// </summary>
+        /// <param name="collection">The collection into which elements will be appended.</param>
+        /// <param name="enumerableToAdd">The IEnumerable containing elements to append to the original collection.</param>
+        /// <typeparam name="T">The type of elements in both collections.</typeparam>
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> enumerableToAdd)
+        {
+            if (collection is IList<T> list && enumerableToAdd is IList<T> listToAdd)
+            {
+                IListRangeExtensions.AddRange(list, listToAdd);
+                return;
+            }
+            else if (collection is IList<T> list1)
+            {
+                IListRangeExtensions.AddRange(list1, enumerableToAdd);
+                return;
+            }
+
+            foreach (T item in enumerableToAdd)
+            {
+                collection.Add(item);
+            }
+        }
+        
         /// <summary>
         /// Appends elements from another collection to the end of the specified collection.
         /// </summary>
@@ -50,6 +75,124 @@ namespace AlastairLundy.DotExtensions.Collections.Generic.ICollections
             foreach (T item in collectionToAdd)
             {
                 collection.Add(item);
+            }
+        }
+
+        
+        public static void InsertRange<T>(this ICollection<T> collection, int index, IEnumerable<T> values)
+        {
+            #region Use IList Optimized Code Path if IList
+            if (collection is IList<T> list)
+            {
+               IListRangeExtensions.InsertRange(list, index, values);
+               return;
+            }
+            #endregion
+            else
+            {
+                int numberOfItemsToBeRemoved = collection.Count - index;
+
+                List<T> removedItems = new();
+
+                int i = 0;
+                foreach (T item in collection)
+                {
+                    if (i >= index && i < collection.Count)
+                    {
+                        removedItems.Add(item);
+                    }
+
+                    i += 1;
+                }
+
+                collection.RemoveRange(index, numberOfItemsToBeRemoved);
+
+                collection.AddRange(values);
+
+                collection.AddRange(removedItems);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="indexes"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static ICollection<T> GetRange<T>(this ICollection<T> collection, IEnumerable<int> indexes)
+        {
+            #region Optimized IList code
+            if (collection is IList<T> list)
+            {
+                List<T> output = new();
+                
+                foreach (int index in indexes)
+                {
+                    output.Add(list[index]);
+                }
+                
+                return output;
+            }
+            #endregion
+            else
+            {
+                List<T> output = new();
+
+                IList<T> collectionList = collection as IList<T> ?? collection.ToArray();
+
+                foreach (int index in indexes)
+                {
+                    output.Add(collectionList[index]);
+                }
+                
+                return output;
+            }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="count"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="IndexOutOfRangeException"></exception>
+        public static ICollection<T> GetRange<T>(this ICollection<T> collection, int startIndex, int count)
+        {
+            int endIndex = startIndex + count;
+            
+            if (endIndex > collection.Count)
+            {
+                throw new ArgumentException(Resources.Exceptions_Enumerables_CountArgumentTooLarge);
+            }
+
+            if (startIndex < 0 || startIndex >= collection.Count)
+            {
+                throw new IndexOutOfRangeException(Resources.Exceptions_IndexOutOfRange
+                    .Replace("{x}", $"{startIndex}")
+                    .Replace("{y}", $"0")
+                    .Replace("{z}", $"{endIndex}"));
+            }
+            
+            #region Optimized IList code
+            if (collection is IList<T> list)
+            {
+               return IListRangeExtensions.GetRange(list, startIndex,  count);
+            }
+            #endregion
+            else
+            {
+                List<int> numbers = new();
+                
+                for (int i = startIndex; i < endIndex; i++)
+                {
+                    numbers.Add(i);
+                }
+                
+                return GetRange(collection, numbers);
             }
         }
 
