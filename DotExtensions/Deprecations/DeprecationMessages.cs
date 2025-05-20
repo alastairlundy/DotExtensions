6ab47550cb6348ff6a332081ef27b4533e85aed9
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-20 11:27:34 +0100

    use block scoped namespace

--- DotExtensions/Deprecations/DeprecationMessages.cs
+++ DotExtensions/Deprecations/DeprecationMessages.cs
@@ -1,6 +1,7 @@
-﻿namespace AlastairLundy.DotExtensions.Deprecations;
-
-internal static class DeprecationMessages
+﻿namespace AlastairLundy.DotExtensions.Deprecations
 {
-    internal const string DeprecationV8 = "This feature is deprecated and will be removed in DotExtensions 8.0";
+    internal static class DeprecationMessages
+    {
+        internal const string DeprecationV8 = "This feature is deprecated and will be removed in DotExtensions 8.0";
+    }
 }
