d57d2e14f13c9ef4a560d7e0f424235005edcfde
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-30 19:09:36 +0100

    start work on Concurrent Range extensions

---
+++ DotExtensions/Collections/Concurrent/ConcurrentRangeExtensions.cs
@@ -0,0 +1,70 @@
+﻿/*
+        MIT License
+       
+       Copyright (c) 2024-2025 Alastair Lundy
+       
+       Permission is hereby granted, free of charge, to any person obtaining a copy
+       of this software and associated documentation files (the "Software"), to deal
+       in the Software without restriction, including without limitation the rights
+       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+       copies of the Software, and to permit persons to whom the Software is
+       furnished to do so, subject to the following conditions:
+       
+       The above copyright notice and this permission notice shall be included in all
+       copies or substantial portions of the Software.
+       
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+       SOFTWARE.
+   */
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace AlastairLundy.DotExtensions.Collections.Concurrent
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ConcurrentRangeExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="concurrentBag"></param>
+        /// <param name="items"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void AddRange<T>(this ConcurrentBag<T> concurrentBag, IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                concurrentBag.Add(item);
+            }
+        }
+    
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="items"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static void AddRange<T>(this IProducerConsumerCollection<T> collection, IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                bool result = collection.TryAdd(item);
+
+                if (result == false)
+                {
+                    throw new InvalidOperationException("Could not add item to the collection.");
+                }
+            }
+        }
+    }
+}
