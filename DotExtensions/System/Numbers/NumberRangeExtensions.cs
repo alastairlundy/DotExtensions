27882679b751a2d5211beae78492a320dae0c5f7
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-05-31 15:22:57 +0100

    start work on Number Range Extensions

---
+++ DotExtensions/System/Numbers/NumberRangeExtensions.cs
@@ -0,0 +1,63 @@
+﻿/*
+        MIT License
+
+       Copyright (c) 2025 Alastair Lundy
+
+       Permission is hereby granted, free of charge, to any person obtaining a copy
+       of this software and associated documentation files (the "Software"), to deal
+       in the Software without restriction, including without limitation the rights
+       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+       copies of the Software, and to permit persons to whom the Software is
+       furnished to do so, subject to the following conditions:
+
+       The above copyright notice and this permission notice shall be included in all
+       copies or substantial portions of the Software.
+
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+       SOFTWARE.
+   */
+
+
+using System;
+using System.Collections.Generic;
+
+namespace AlastairLundy.DotExtensions.Ints;
+
+public static class NumberRangeExtensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public static IEnumerable<int> IntegerRangeAsEnumerable(this int start, int count)
+    {
+        List<int> list = new List<int>();
+
+        for (int i = start; i < count; i++)
+        {
+            list.Add(i);
+        }
+        
+        return list;
+    }
+
+#if NETSTANDARD2_1 || NET5_0_OR_GREATER
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="count"></param>
+    /// <returns></returns>
+    public static Range AsRange(this int start, int count)
+    {
+        return new Range(start, count);
+    }
+#endif
+}
