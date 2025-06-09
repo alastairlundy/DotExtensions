cac0a7e27ef056c12208be47934b62a5dcfc037e
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-06-09 11:34:23 +0100

    add xml doc comments

--- DotExtensions/System/Numbers/NumberRangeExtensions.cs
+++ DotExtensions/System/Numbers/NumberRangeExtensions.cs
@@ -22,20 +22,24 @@
        SOFTWARE.
    */
 
-
+#if NET5_0_OR_GREATER || NETSTANDARD2_1
 using System;
+#endif
 using System.Collections.Generic;
 
-namespace AlastairLundy.DotExtensions.Ints;
+namespace AlastairLundy.DotExtensions.Numbers;
 
+/// <summary>
+/// Provides static extension methods for working with number ranges.
+/// </summary>
 public static class NumberRangeExtensions
 {
     /// <summary>
-    /// 
+    /// Returns an enumerable sequence of integers from <paramref name="start"/> up to start + count.
     /// </summary>
-    /// <param name="start"></param>
-    /// <param name="count"></param>
-    /// <returns></returns>
+    /// <param name="start">The starting integer of the sequence.</param>
+    /// <param name="count">The number of integers to generate.</param>
+    /// <returns>An IEnumerable sequence of integers from the start index up to count.</returns>
     public static IEnumerable<int> IntegerRangeAsEnumerable(this int start, int count)
     {
         List<int> list = new List<int>();
@@ -50,22 +54,22 @@ public static class NumberRangeExtensions
 
 #if NETSTANDARD2_1 || NET5_0_OR_GREATER
     /// <summary>
-    /// 
+    /// Creates a new instance of the Range struct based on the given start value and length.
     /// </summary>
-    /// <param name="start"></param>
-    /// <param name="count"></param>
-    /// <returns></returns>
+    /// <param name="start">The starting index or integer value of the range.</param>
+    /// <param name="count">The number of elements in the range.</param>
+    /// <returns>A new Range instance representing the specified sequence of values.</returns>
     public static Range AsRange(this int start, int count)
     {
         return new Range(start, count);
     }
     
     /// <summary>
-    /// 
+    /// Creates a new instance of the Range struct based on the given Index value and length.
     /// </summary>
-    /// <param name="start"></param>
-    /// <param name="count"></param>
-    /// <returns></returns>
+    /// <param name="start">The starting Index value.</param>
+    /// <param name="count">The number of elements in the range.</param>
+    /// <returns>A new Range instance representing the specified sequence of values.</returns>
     public static Range AsRange(this Index start, int count)
     {
         return new Range(start, count);
