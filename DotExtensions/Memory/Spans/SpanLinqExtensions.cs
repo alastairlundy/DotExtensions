078f2baac4f609dbb848888724e0d9c018d0df9c
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-07-05 23:16:20 +0100

    Update SpanLinqExtensions.cs

--- DotExtensions/Memory/Spans/SpanLinqExtensions.cs
+++ DotExtensions/Memory/Spans/SpanLinqExtensions.cs
@@ -26,7 +26,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using AlastairLundy.DotExtensions.Collections.Generic.Pairs.KeyValuePairs;
+
 using AlastairLundy.DotExtensions.Localizations;
 using AlastairLundy.DotPrimitives.Collections.Groupings;
 
