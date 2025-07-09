a2aeb83f0a63d4cc6cc40662d88f4c6d4087f388
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-07-09 11:51:06 +0100

    fix issue with .NET Framework test

--- DotExtensions.Tests/TestData/LoremWordsTestData.cs
+++ DotExtensions.Tests/TestData/LoremWordsTestData.cs
@@ -8,11 +8,28 @@ namespace SystemExtensions.Tests.TestData;
 public class LoremWordsTestData : IEnumerable<object[]>
 {
     private Lorem _lorem = new();
+    
+    #if NETFRAMEWORK
+    private readonly Random _random;
+    #endif
+
+    public LoremWordsTestData()
+    {
+        #if NETFRAMEWORK
+        _random = new Random();
+#endif
+    }
+    
     public IEnumerator<object[]> GetEnumerator()
     {
         for (int i = 0; i < 20; i++)
         {
+            #if NET5_0_OR_GREATER
             yield return new object[] { string.Join(" ", _lorem.Words(Random.Shared.Next(1, 10))) };
+            #else
+            yield return new object[] { string.Join(" ", _lorem.Words(_random.Next(1, 10))) };
+
+            #endif
         }
     }
 
