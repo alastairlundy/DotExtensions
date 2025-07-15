3d1bfdc2d19eb885ff6df9c9e8957f3d2357e88d
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-07-15 22:00:28 +0100

    Update SpanAsEnumerableExtensions.cs

--- DotExtensions/Memory/Spans/SpanAsEnumerableExtensions.cs
+++ DotExtensions/Memory/Spans/SpanAsEnumerableExtensions.cs
@@ -37,7 +37,7 @@ public static class SpanAsEnumerableExtensions
     /// </summary>
     /// <remarks>This method works by creating a new Array of the same length as the span, copies files to the array, and returns it as an IEnumerable.
     /// <para>Conversion from a <see cref="Span{T}"/> to an <see cref="IEnumerable{T}"/> should not be done if performance is a concern.
-    /// Spans are faster to iterate over and work with than IEnumerables.</para>
+    /// Spans are faster to iterate over and work with than IEnumerable.</para>
     /// </remarks>
     /// <param name="span">The span to convert.</param>
     /// <typeparam name="T">The type of elements in the span.</typeparam>
