2b142a9fd737b5742415ee958b15fdd5f130b874
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-07-16 23:14:40 +0100

    add xml doc comments

--- DotExtensions/Memory/Spans/SpanLinqExtensions.cs
+++ DotExtensions/Memory/Spans/SpanLinqExtensions.cs
@@ -42,12 +42,13 @@ namespace AlastairLundy.DotExtensions.Memory.Spans;
 /// </summary>
 public static class SpanLinqExtensions
 {
+
     /// <summary>
-    /// 
+    /// Applies the given action to each element of this Span.
     /// </summary>
-    /// <param name="target"></param>
-    /// <param name="action"></param>
-    /// <typeparam name="T"></typeparam>
+    /// <param name="action">The action to apply to each element in the span.</param>
+    /// <param name="target">The span to apply the elements to.</param>
+    /// <typeparam name="T">The type of items in the Span.</typeparam>
     public static void ForEach<T>(this ref Span<T> target, Action<T> action)
     {
         for (int index = 0; index < target.Length; index++)
@@ -55,16 +56,16 @@ public static class SpanLinqExtensions
             action.Invoke(target[index]);
         }
     }
-    
+
     /// <summary>
-    /// 
+    /// Applies the given func to each element of this Span.
     /// </summary>
-    /// <param name="target"></param>
-    /// <param name="action"></param>
-    /// <typeparam name="T"></typeparam>
+    /// <param name="target">The span to apply the elements to.</param>
+    /// <param name="action">The func to apply to each element in the span.</param>
+    /// <typeparam name="T">The type of items in the Span.</typeparam>
     public static void ForEach<T>(this Span<T> target, Func<T, T> action)
     {
-        for(int i = 0; i < target.Length; i++)
+        for (int i = 0; i < target.Length; i++)
         {
             target[i] = action.Invoke(target[i]);
         }
