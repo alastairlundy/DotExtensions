6ce132f69a3bd6fe0be8c4f10d71652179539d32
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-07-31 23:31:58 +0200

    remove Deprecation static class

--- DotExtensions/Deprecations/DeprecationMessages.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AlastairLundy.DotExtensions.Deprecations;
-
-internal static class DeprecationMessages
-{
-    internal const string DeprecationV9 = "This feature is deprecated and will be removed in DotExtensions 9.0";
-}
--- DotExtensions/Linq/Immediate/EnumerableIndexExtensions.cs
+++ DotExtensions/Linq/Immediate/EnumerableIndexExtensions.cs
@@ -1,18 +1,18 @@
 ﻿/*
         MIT License
-       
+
        Copyright (c) 2024-2025 Alastair Lundy
-       
+
        Permission is hereby granted, free of charge, to any person obtaining a copy
        of this software and associated documentation files (the "Software"), to deal
        in the Software without restriction, including without limitation the rights
        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
        copies of the Software, and to permit persons to whom the Software is
        furnished to do so, subject to the following conditions:
-       
+
        The above copyright notice and this permission notice shall be included in all
        copies or substantial portions of the Software.
-       
+
        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,35 +23,48 @@
    */
 
 using System.Collections.Generic;
-using System.Linq;
+using AlastairLundy.DotExtensions.Exceptions;
+#if NET8_0_OR_GREATER
+using System.Diagnostics.CodeAnalysis;
+#endif
 
 // ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable SuggestVarOrType_BuiltInTypes
 
-namespace AlastairLundy.DotExtensions.Collections.Generic.Enumerables;
+namespace AlastairLundy.DotExtensions.Linq.Immediate;
 
 /// <summary>
-/// Provides static methods for finding and removing duplicates from an IEnumerable.
+/// 
 /// </summary>
-public static class EnumerableDeDuplicateExtensions
+public static class EnumerableIndexExtensions
 {
     /// <summary>
-    /// Returns whether an <see cref="IEnumerable{T}"/> contains duplicate instances of an object.
+    /// Returns the index of an object in an IEnumerable.
     /// </summary>
-    /// <param name="source">The <see cref="IEnumerable{T}"/> to be searched.</param>
-    /// <typeparam name="T">The type of objects in the <see cref="IEnumerable{T}"/>.</typeparam>
-    /// <returns>True if the <see cref="IEnumerable{T}"/> contains duplicate objects; false otherwise.</returns>
-    public static bool ContainsDuplicates<T>(this IEnumerable<T> source) where T : notnull
+    /// <param name="source">The IEnumerable to be searched.</param>
+    /// <param name="obj">The object to get the index of.</param>
+    /// <typeparam name="T">The type of object in the IEnumerable.</typeparam>
+    /// <returns>The index of an object in an IEnumerable, if the IEnumerable contains the object, throws an exception otherwise.</returns>
+    /// <exception cref="ValueNotFoundException">Thrown if the IEnumerable does not contain the specified object.</exception>
+    public static int IndexOf<T>(this IEnumerable<T> source, T obj)
     {
-        HashSet<T> hash = new();
-        
+        if (source is IList<T> list)
+        {
+            return list.IndexOf(obj);
+        }
+            
+        int index = 0;
+                
         foreach (T item in source)
         {
-           bool result = hash.Add(item);
-
-           if (result == false)
-               return true;
+            if (item is not null && item.Equals(obj))
+            {
+                return index;
+            }
+                    
+            index++;
         }
-
-        return false;
+        
+        return -1;
     }
 }
