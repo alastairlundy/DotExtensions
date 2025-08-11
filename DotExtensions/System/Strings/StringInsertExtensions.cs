b92eb80c89a8ac52f182f9accf048b243ae92085
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-11 23:10:14 +0200

    add new string insert char method

---
+++ DotExtensions/System/Strings/StringInsertExtensions.cs
@@ -0,0 +1,67 @@
+/*
+        MIT License
+
+       Copyright (c) 2020-2025 Alastair Lundy
+
+       Permission is hereby granted, free of charge, to any person obtaining a copy
+       of this software and associated documentation files (the "Software"), to deal
+       in the Software without restriction, including without limitation the rights
+       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+       copies of the Software, and to permit persons to whom the Software is
+       furnished to do so, subject to the following conditions:
+
+       The above copyright notice and this permission notice shall be included in all
+       copies or substantial portions of the Software.
+
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+       SOFTWARE.
+   */
+
+using System;
+using System.Text;
+
+namespace AlastairLundy.DotExtensions.Strings;
+
+/// <summary>
+/// 
+/// </summary>
+public static class StringInsertExtensions
+{
+    /// <summary>
+    /// Inserts a character in a string at a specified index.
+    /// </summary>
+    /// <param name="str">The string to insert a character into.</param>
+    /// <param name="index">The index to insert the new character at.</param>
+    /// <param name="c">The character to insert in the string.</param>
+    /// <returns>The updated string with the inserted character at the specified index.</returns>
+    public static string Insert(this string str, int index, char c)
+    {
+        if (index < 0 || index >= str.Length)
+            throw new IndexOutOfRangeException();
+        
+        if (str[index] == c)
+            return str;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int start = 0;
+        
+        if (index > 1)
+        {
+            stringBuilder.Append(str.Substring(0, index - 1));
+            start = index;
+        }
+        
+        for (int i = start; i < str.Length; i++)
+        {
+            stringBuilder.Append(i == index ? c : str[i]);
+        }
+        
+        return stringBuilder.ToString();
+    }
+}
