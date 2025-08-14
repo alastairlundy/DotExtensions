35a1a141420fe0b790377ab0b081198f811043b9
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-14 18:04:39 +0200

    merge and re-write string case extensions

--- DotExtensions/System/Strings/Cases/CaseExtensions.cs
+++ DotExtensions/System/Strings/Cases/CaseExtensions.cs
@@ -1,18 +1,18 @@
 ﻿/*
         MIT License
-       
+
        Copyright (c) 2020-2025 Alastair Lundy
-       
+
        Permission is hereby granted, free of charge, to any person obtaining a copy
        of this software and associated documentation files (the "Software"), to deal
        in the Software without restriction, including without limitation the rights
        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
        copies of the Software, and to permit persons to whom the Software is
        furnished to do so, subject to the following conditions:
-       
+
        The above copyright notice and this permission notice shall be included in all
        copies or substantial portions of the Software.
-       
+
        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,15 +24,31 @@
 
 // ReSharper disable CheckNamespace
 
+using System.Linq;
+// ReSharper disable ConvertClosureToMethodGroup
+
 namespace AlastairLundy.DotExtensions.Strings;
 
-public static class LowerCaseExtensions
+/// <summary>
+/// 
+/// </summary>
+public static class CaseExtensions
 {
+
+    /// <summary>
+    /// Returns whether a string is upper case or not.
+    /// </summary>
+    /// <param name="s">The string to be checked.</param>
+    /// <returns>True if the string is upper case; false otherwise.</returns>
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static bool IsUpperCase(this string s)
+        => s.All(x => char.IsUpper(x));
+    
     /// <summary>
     /// Returns whether a string is lower case or not.
     /// </summary>
     /// <param name="s">The string to be checked.</param>
     /// <returns>True if a string is lower case; false otherwise.</returns>
-    public static bool IsLowerCase(this string s) 
-        => s.Equals(s.ToLower());
+    public static bool IsLowerCase(this string s)
+        => s.All(x => char.IsLower(x));
 }
