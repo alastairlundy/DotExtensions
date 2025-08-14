0362a043ea5b948afdc06801db117b4032533068
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-08-14 17:43:32 +0200

    throw an exception if in ToCharArray, ToList, or AsEnumerable StringSegment is null or empty

--- DotExtensions.MsExtensions/StringSegments/SegmentToCharsExtensions.cs
+++ DotExtensions.MsExtensions/StringSegments/SegmentToCharsExtensions.cs
@@ -22,7 +22,11 @@
        SOFTWARE.
    */
 
+using System;
 using System.Collections.Generic;
+
+using AlastairLundy.DotExtensions.MsExtensions.Localizations;
+
 using Microsoft.Extensions.Primitives;
 
 namespace AlastairLundy.DotExtensions.MsExtensions.StringSegments;
@@ -34,8 +38,13 @@ public static class SegmentToCharsExtensions
     /// </summary>
     /// <param name="segment">The string segment to enumerate.</param>
     /// <returns>The <see cref="IEnumerable{T}"/> of chars from the <see cref="StringSegment"/>.</returns>
+    /// <exception cref="ArgumentException">Thrown if the StringSegment is null or empty.</exception>
     public static IEnumerable<char> AsEnumerable(this StringSegment segment)
     {
+        if (StringSegment.IsNullOrEmpty(segment))
+            throw new ArgumentException(Resources.Exceptions_Arguments_Segment_Empty);
+
+        
         for (int i = 0; i < segment.Length; i++)
         {
             yield return segment[i];
@@ -47,8 +56,12 @@ public static class SegmentToCharsExtensions
     /// </summary>
     /// <param name="segment">The string segment to enumerate.</param>
     /// <returns>The string segment as a char array.</returns>
+    /// <exception cref="ArgumentException">Thrown if the StringSegment is null or empty.</exception>
     public static char[] ToCharArray(this StringSegment segment)
     {
+        if (StringSegment.IsNullOrEmpty(segment))
+            throw new ArgumentException(Resources.Exceptions_Arguments_Segment_Empty);
+
         char[] charArray = new char[segment.Length];
 
         for (int i = 0; i < segment.Length; i++)
@@ -63,10 +76,13 @@ public static class SegmentToCharsExtensions
     /// Returns the <see cref="StringSegment"/> as a List of type <see cref="char"/>.
     /// </summary>
     /// <param name="segment">The segment to enumerate as a list.</param>
-    /// <returns>A list of characters from the StringSegment if any characters are in the StringSegment,
-    /// an empty list otherwise.</returns>
+    /// <returns>A list of characters from the StringSegment if any characters are in the StringSegment.</returns>
+    /// <exception cref="ArgumentException">Thrown if the StringSegment is null or empty.</exception>
     public static List<char> ToList(this StringSegment segment)
     {
+        if (StringSegment.IsNullOrEmpty(segment))
+            throw new ArgumentException(Resources.Exceptions_Arguments_Segment_Empty);
+        
         List<char> list = new List<char>();
             
         for (int i = 0; i < segment.Length; i++)
