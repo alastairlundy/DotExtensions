b4dbde780fcfe4ef982040834b069ed1c9742d4c
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-09-07 12:13:42 +0100

    Create CharacterConstants.cs

---
+++ DotExtensions.Tests/TestData/CharacterConstants.cs
@@ -0,0 +1,52 @@
+﻿/*
+        MIT License
+       
+       Copyright (c) 2020-2025 Alastair Lundy
+       
+       Permission is hereby granted, free of charge, to any person obtaining a copy
+       of this software and associated documentation files (the "Software"), to deal
+       in the Software without restriction, including without limitation the rights
+       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+       copies of the Software, and to permit persons to whom the Software is
+       furnished to do so, subject to the following conditions:
+       
+       The above copyright notice and this permission notice shall be included in all
+       copies or substantial portions of the Software.
+       
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+       SOFTWARE.
+   */
+
+// ReSharper disable ConvertToAutoProperty
+
+namespace DotExtensions.Tests.TestData;
+
+/// <summary>
+/// 
+/// </summary>
+internal static class CharacterConstants
+{
+    private static readonly char[] SpecialChars =
+    [
+        ',', '.', '\\', '/', '^', '*', '&', '?', '!', '#', '~', '_', '+',
+        '-', '@', '<', '>', '=', '(', ')', '%', '$', '£', '"', ';', ':', '{', '}', '[', ']'
+    ];
+        
+    private static readonly string[] EscapeChars = 
+        ["\r", "\n", "\t", "\v", @"\c", @"\e", "\f", "\a", "\b",  @"\NNN", @"\xHH", "\\"];
+        
+    /// <summary>
+    /// 
+    /// </summary>
+    internal static char[] SpecialCharacters => SpecialChars;
+        
+    /// <summary>
+    /// 
+    /// </summary>
+    internal static string[] EscapeCharacters => EscapeChars;
+}
