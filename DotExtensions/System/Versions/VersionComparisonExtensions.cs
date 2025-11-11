a5e4ba0c3c5917cac168be7402934714090b242c
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-11-11 20:08:01 +0000

    rename version extensions add new Version static extension member methods

--- DotExtensions/System/Versions/VersionComparisonExtensions.cs
+++ DotExtensions/System/Versions/VersionComparisonExtensions.cs
@@ -32,29 +32,65 @@ namespace AlastairLundy.DotExtensions.Versions;
 public static class VersionComparisonExtensions
 {
     /// <summary>
-    /// Returns whether the specified version is newer than or equal to the current version object.
+    /// 
     /// </summary>
     /// <param name="version">The current version object.</param>
-    /// <param name="versionToBeCompared">The version to be compared.</param>
-    /// <returns>True if the specified compared version is newer than or equal to the current version, and returns false otherwise.</returns>
-    public static bool IsAtLeastVersion(this Version version, Version versionToBeCompared) 
-        => versionToBeCompared >= version;
+    extension(Version version)
+    {
+        /// <summary>
+        /// Returns whether this version is newer than or equal to the current version object.
+        /// </summary>
+        /// <param name="versionToBeCompared">The version to be compared.</param>
+        /// <returns>True, if the specified compared version is newer than or equal to this version, returns false otherwise.</returns>
+        public bool IsAtLeast(Version versionToBeCompared) 
+            => versionToBeCompared >= version;
+
+        /// <summary>
+        /// Returns whether this version is older than the current Version object.
+        /// </summary>
+        /// <param name="versionToBeCompared">The version to be compared.</param>
+        /// <returns>True, if the specified compared version is older than this version, returns false otherwise.</returns>
+        public bool IsOlderThan(Version versionToBeCompared) 
+            => versionToBeCompared < version;
+
+        /// <summary>
+        /// Returns whether this version is newer than the current version object.
+        /// </summary>
+        /// <param name="versionToBeCompared">The version to be compared.</param>
+        /// <returns>True, if the specified compared version is newer than this version, returns false otherwise.</returns>
+        public bool IsNewerThan(Version versionToBeCompared) 
+            => version > versionToBeCompared;
+    }
 
     /// <summary>
-    /// Returns whether a specified Version is older than the current Version object.
+    /// Provides extension methods for performing version comparison operations on <see cref="Version"/> objects.
     /// </summary>
-    /// <param name="version">The current version object.</param>
-    /// <param name="versionToBeCompared">The version to be compared.</param>
-    /// <returns>True if the specified compared version is older than the current version, and returns false otherwise.</returns>
-    public static bool IsOlderThanVersion(this Version version, Version versionToBeCompared) 
-        => versionToBeCompared < version;
+    extension(Version)
+    {
+        /// <summary>
+        /// Compares two versions and returns the newer version.
+        /// </summary>
+        /// <param name="versionA">The first version to compare.</param>
+        /// <param name="versionB">The second version to compare.</param>
+        /// <returns>The newer of the two specified versions.</returns>
+        public static Version GetNewerVersion(Version versionA, Version versionB)
+        {
+            if (versionA > versionB)
+                return versionA;
+            return versionB;
+        }
 
-    /// <summary>
-    /// Returns whether the specified version is newer than the current version object.
-    /// </summary>
-    /// <param name="version">The current version object.</param>
-    /// <param name="versionToBeCompared">The version to be compared.</param>
-    /// <returns>True if the specified compared version is newer than the current version, and returns false otherwise.</returns>
-    public static bool IsNewerThanVersion(this Version version, Version versionToBeCompared) 
-        => version > versionToBeCompared;
+        /// <summary>
+        /// Compares two versions and returns the older version.
+        /// </summary>
+        /// <param name="versionA">The first version to compare.</param>
+        /// <param name="versionB">The second version to compare.</param>
+        /// <returns>The older of the two specified versions.</returns>
+        public static Version GetOlderVersion(Version versionA, Version versionB)
+        {
+            if (versionA < versionB)
+                return versionA;
+            return versionB;
+        }
+    }
 }
