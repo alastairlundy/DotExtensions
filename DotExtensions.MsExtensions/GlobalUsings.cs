2eb0bff974d1f8f5eca9a5114ca914fabf1941a6
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-11-22 22:09:51 +0000

    Update GlobalUsings.cs

--- DotExtensions.MsExtensions/GlobalUsings.cs
+++ DotExtensions.MsExtensions/GlobalUsings.cs
@@ -2,6 +2,8 @@
 
 global using System;
 global using System.Collections.Generic;
+global using System.Linq;
 global using System.Text;
+global using AlastairLundy.DotExtensions.MsExtensions.Exceptions;
 global using AlastairLundy.DotExtensions.MsExtensions.Internal.Localizations;
 global using Microsoft.Extensions.Primitives;
