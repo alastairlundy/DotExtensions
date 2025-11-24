9c49f24f2a7cf9acae737f3005369522f47cb576
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-11-24 18:05:43 +0000

    remove AoT unsafe PlatformNotSupported Throw helpers for now

--- DotExtensions/System/Exceptions/PlatformNotSupportedExtensions.General.cs
+++ DotExtensions/System/Exceptions/PlatformNotSupportedExtensions.General.cs
@@ -23,12 +23,9 @@
    */
 
 using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
+
 // ReSharper disable InconsistentNaming
 
 namespace AlastairLundy.DotExtensions.Exceptions;
@@ -87,59 +84,7 @@ public static partial class PlatformNotSupportedExceptionExtensions
             throw new PlatformNotSupportedException();
         }
 
-        /// <summary>
-        /// Throws a <see cref="PlatformNotSupportedException"/> if the current platform matches any platform
-        /// specified by <see cref="UnsupportedOSPlatformAttribute"/> applied to the declaring type.
-        /// </summary>
-        /// <remarks>
-        /// This method relies on Reflection to determine whether the platform is unsupported based on
-        /// the <see cref="UnsupportedOSPlatformAttribute"/> declared on the type. It is not suitable for
-        /// use in AoT (Ahead-of-Time) compiled environments or trimmed assemblies due to its reliance on Reflection.
-        /// </remarks>
-        /// <exception cref="PlatformNotSupportedException">
-        /// Thrown when the current platform matches an unsupported platform defined
-        /// by the <see cref="UnsupportedOSPlatformAttribute"/> applied to the declaring type.
-        /// If the attribute specifies a message, it will be included in the exception.
-        /// </exception>
-        /// <exception cref="RequiresUnreferencedCodeAttribute">
-        /// Indicates that this method requires code that cannot be analysed statically for compatibility
-        /// with AoT or assembly trimming scenarios due to the use of Reflection.
-        /// </exception>
-        [RequiresUnreferencedCode(
-            "This method requires the use of Reflection, which is not supported on AoT and may not work as intended on Trimmed Assemblies.")]
-        public static void ThrowIfUnSupportedPlatform()
-        {
-            MethodBase? method;
-            try
-            {
-                method = MethodBase.GetCurrentMethod();
-            }
-            catch (TargetException)
-            {
-                // This member was invoked with a late-binding mechanism.
-                return;
-            }
-
-            Type? type = method?.DeclaringType;
-
-            if (method is null || type is null)
-                return;
-
-            UnsupportedOSPlatformAttribute[] unsupportedOsPlatformAttributes =
-               (UnsupportedOSPlatformAttribute[])Attribute.GetCustomAttributes(type, typeof(UnsupportedOSPlatformAttribute));
-            
-            foreach (UnsupportedOSPlatformAttribute unsupportedPlatform in unsupportedOsPlatformAttributes)
-            {
-                if (OperatingSystem.IsOSPlatform(unsupportedPlatform.PlatformName))
-                {
-                    if(unsupportedPlatform.Message is not null)
-                        throw new PlatformNotSupportedException(unsupportedPlatform.Message);
-                    
-                    throw new PlatformNotSupportedException();
-                }
-            }
-        }
-
+        
         /// <summary>
         /// Throws a <see cref="PlatformNotSupportedException"/> if the current platform matches
         /// any of the unsupported platforms specified in the <paramref name="unsupportedOsPlatforms"/> array.
@@ -180,61 +125,7 @@ public static partial class PlatformNotSupportedExceptionExtensions
                 }
             }
         }
-
-        /// <summary>
-        /// Throws a <see cref="PlatformNotSupportedException"/> if the current platform
-        /// is not among the supported platforms defined by the <see cref="SupportedOSPlatformAttribute"/>
-        /// applied to the declaring type of the method.
-        /// </summary>
-        /// <param name="exceptionMessage">
-        /// An optional custom exception message to include in the thrown <see cref="PlatformNotSupportedException"/>.
-        /// If not provided, a default message is used.
-        /// </param>
-        /// <exception cref="PlatformNotSupportedException">
-        /// Thrown when the current platform is not supported based on
-        /// <see cref="SupportedOSPlatformAttribute"/> annotations.
-        /// </exception>
-        [RequiresUnreferencedCode(
-            "This method requires the use of Reflection, which is not supported on AoT and may not work as intended on Trimmed Assemblies.")]
-        public static void ThrowIfNotSupportedPlatform(string? exceptionMessage = null)
-        {
-            MethodBase? method;
-
-            #if NET8_0_OR_GREATER
-            if (!RuntimeFeature.IsDynamicCodeSupported)
-                return;
-            #endif
-            
-            try
-            {
-                method = MethodBase.GetCurrentMethod();
-            }
-            catch (TargetException)
-            {
-                // This member was invoked with a late-binding mechanism.
-                return;
-            }
-
-            Type? type = method?.DeclaringType;
-
-            if (method is null || type is null)
-                return;
-
-            SupportedOSPlatformAttribute[] supportedOsPlatformAttributes =
-                (SupportedOSPlatformAttribute[])Attribute.GetCustomAttributes(type,
-                    typeof(SupportedOSPlatformAttribute));
-
-            bool osNotSupported = supportedOsPlatformAttributes.All(p => !OperatingSystem.IsOSPlatform(p.PlatformName));
-
-            if (osNotSupported)
-            {
-                if(exceptionMessage is not null)
-                    throw new PlatformNotSupportedException(exceptionMessage);
-                    
-                throw new PlatformNotSupportedException();
-            }
-        }
-
+        
         /// <summary>
         /// Throws a <see cref="PlatformNotSupportedException"/> if the current platform does not match any of the
         /// specified supported OS platforms.
