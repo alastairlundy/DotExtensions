99f7710a6d2a9b5c76e671552823933cb6fdd64d
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-12-13 20:31:52 +0000

    add Memory Sort

--- DotExtensions.Memory/GlobalUsings.cs
+++ DotExtensions.Memory/GlobalUsings.cs
@@ -1,5 +1,6 @@
 // Global using directives
 
 global using System;
+global using System.Buffers;
 global using System.Linq;
 global using AlastairLundy.DotExtensions.Memory.Internal.Localizations;
--- DotExtensions.Memory/Spans/SpanResizeExtensions.cs
+++ DotExtensions.Memory/Spans/SpanResizeExtensions.cs
@@ -22,8 +22,6 @@
        SOFTWARE.
    */
 
-using System.Buffers;
-
 namespace AlastairLundy.DotExtensions.Memory.Spans;
 
 /// <summary>
