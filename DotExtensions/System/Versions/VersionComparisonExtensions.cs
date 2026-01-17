846c92f6d63cd759f2ee4d611f877ee1b8c2ea1e
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-17 16:27:13 +0000

    to extension method invokation

--- DotExtensions/System/Versions/VersionComparisonExtensions.cs
+++ DotExtensions/System/Versions/VersionComparisonExtensions.cs
@@ -67,7 +67,7 @@ public static class VersionComparisonExtensions
         /// <returns>The newer of the two specified versions.</returns>
         [Obsolete(DeprecationMessages.DeprecationV10 + " . Use Version.Max() instead.")]
         public static Version GetNewerVersion(Version versionA, Version versionB)
-            => Max(versionA, versionB);
+            => Version.Max(versionA, versionB);
 
         /// <summary>
         /// Compares two version objects and returns the higher version number.
@@ -90,7 +90,7 @@ public static class VersionComparisonExtensions
         /// <returns>The older of the two specified versions.</returns>
         [Obsolete(DeprecationMessages.DeprecationV10 + " . Use Version.Min() instead.")]
         public static Version GetOlderVersion(Version versionA, Version versionB)
-            => Min(versionA, versionB);
+            => Version.Min(versionA, versionB);
 
         /// <summary>
         /// Compares two versions and returns the older version.
