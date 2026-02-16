88ef49ede1da7feb9db481e4f266b13f67e913e2
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-02-16 15:52:29 +0000

    deprecate method for removal in v10

---
+++ DotExtensions.Memory/Internal/DeprecationMessages.cs
@@ -0,0 +1,7 @@
+namespace DotExtensions.Memory.Internal;
+
+internal static class DeprecationMessages
+{
+    internal const string DeprecationV10 =
+        "This feature is deprecated and will be removed in version 10";
+}
