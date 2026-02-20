4f6bfe215f7ce84ced6c410e85752ee0e05aa7df
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-02-20 14:12:56 +0000

    fix file size extension unit test

--- DotExtensions.Tests/IO/FileSizeExtensionTests.cs
+++ DotExtensions.Tests/IO/FileSizeExtensionTests.cs
@@ -1,28 +1,4 @@
-﻿/*
-        MIT License
-       
-       Copyright (c) 2026 Alastair Lundy
-       
-       Permission is hereby granted, free of charge, to any person obtaining a copy
-       of this software and associated documentation files (the "Software"), to deal
-       in the Software without restriction, including without limitation the rights
-       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-       copies of the Software, and to permit persons to whom the Software is
-       furnished to do so, subject to the following conditions:
-       
-       The above copyright notice and this permission notice shall be included in all
-       copies or substantial portions of the Software.
-       
-       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-       SOFTWARE.
-   */
-
-using System;
+﻿using System;
 using System.IO;
 using DotExtensions.IO;
 
@@ -42,10 +18,10 @@ public class FileSizeExtensionTests
     {
         string actualFileSizeString = _testFile.GetFileSizeString();
 
-        int end = actualFileSizeString
-            .LastIndexOfAny(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']);
+        string fileSizeUnitString = _testFile.GetFileSizeUnitString();
         
-        long actualFileSize = long.Parse(actualFileSizeString.Substring(0, Math.Abs(0 - end)));
+        long actualFileSize = long.Parse(actualFileSizeString.Substring(0,
+            actualFileSizeString.Length - fileSizeUnitString.Length - 1));
         
         await Assert.That(actualFileSizeString)
             .IsNotEmpty();
