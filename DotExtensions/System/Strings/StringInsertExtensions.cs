fd7796c270856943e95a8328e23c794e2a3bf600
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-02-20 14:43:06 +0000

    Update StringInsertExtensions.cs

--- DotExtensions/System/Strings/StringInsertExtensions.cs
+++ DotExtensions/System/Strings/StringInsertExtensions.cs
@@ -31,9 +31,6 @@ namespace DotExtensions.Strings;
 /// </summary>
 public static class StringInsertExtensions
 {
-    /// <summary>
-    ///
-    /// </summary>
     /// <param name="str">The string to insert characters into.</param>
     extension(string str)
     {
