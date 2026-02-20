233c58a58583536785bf51014628c641b5e4c001
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-02-20 14:52:52 +0000

    Update Program.cs

--- DotExtensions.Benchmarking/Program.cs
+++ DotExtensions.Benchmarking/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 using BenchmarkDotNet.Running;
-using DotExtensions.Benchmarking.Benchmarks.IO;
+//using DotExtensions.Benchmarking.Benchmarks.IO;
 
 //using DotExtensions.Benchmarking.Benchmarks.System.Versions;
 
-//BenchmarkRunner.Run(assembly: Assembly.GetCallingAssembly());
+BenchmarkRunner.Run(assembly: Assembly.GetCallingAssembly());
 
-BenchmarkRunner.Run<RandomFileRetrievalBenchmark>();
+//BenchmarkRunner.Run<RandomFileRetrievalBenchmark>();
 //BenchmarkRunner.Run<VersionGracefulParseBenchmarks>();
