e2a003d6c92ceb748ffa837ce35512fc5812d1cc
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-02-23 16:22:11 +0000

    restructure Week of code and DateOnly support

--- DotExtensions/System/Dates/WeekOfExtensions.cs
+++ DotExtensions/System/Dates/WeekOfExtensions.cs
@@ -31,6 +31,56 @@ namespace DotExtensions.Dates;
 /// </summary>
 public static class WeekOfExtensions
 {
+    private static int InternalWeekOfMonthCount(DateTime date, int year, int month)
+    {
+        DayOfWeek firstDayOfWeek = CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
+        int daysInMonth = DateTime.DaysInMonth(year, month);
+
+        int weekCount = 0;
+
+        for (int day = 1; day < daysInMonth; day++)
+        {
+            DateTime currentDate = new(year, month, day);
+
+            if (currentDate.DayOfWeek == firstDayOfWeek)
+                weekCount++;
+
+            if (currentDate.Day == date.Day)
+                break;
+        }
+
+        return weekCount;
+    }
+    
+    private static int InternalWeekOfYearCount(DateTime date, CalendarWeekRule calendarWeekRule, int year, int month)
+    {
+        DayOfWeek firstDayOfWeek = CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
+        int daysInYear = CultureInfo.CurrentCulture.Calendar.IsLeapYear(year) ? 366 : 365;
+        int weekCount = 0;
+
+        for (int day = 1; day < daysInYear; day++)
+        {
+            DateTime currentDate = new(year, month, day);
+
+            if (day == 1 && calendarWeekRule == CalendarWeekRule.FirstDay)
+                weekCount = 1;
+            else if (day == 4 && calendarWeekRule == CalendarWeekRule.FirstFourDayWeek)
+                weekCount = 1;
+            else if (day == 7 && calendarWeekRule == CalendarWeekRule.FirstFullWeek)
+                weekCount = 1;
+            else
+            {
+                if (currentDate.DayOfWeek == firstDayOfWeek)
+                    weekCount++;
+            }
+
+            if (currentDate.Day == date.Day)
+                break;
+        }
+
+        return weekCount;
+    }
+    
     /// <param name="date">The date to be used.</param>
     extension(DateTime date)
     {
@@ -40,23 +90,7 @@ public static class WeekOfExtensions
         /// <returns>The week number in a given month.</returns>
         public int WeekOfMonth()
         {
-            DayOfWeek firstDayOfWeek = CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
-            int daysInMonth = DateTime.DaysInMonth(date.Year, date.Month);
-
-            int weekCount = 0;
-
-            for (int day = 1; day < daysInMonth; day++)
-            {
-                DateTime currentDate = new(date.Year, date.Month, day);
-
-                if (currentDate.DayOfWeek == firstDayOfWeek)
-                    weekCount++;
-
-                if (currentDate.Day == date.Day)
-                    break;
-            }
-
-            return weekCount;
+            return InternalWeekOfMonthCount(date, date.Year, date.Month);
         }
 
         /// <summary>
@@ -66,31 +100,32 @@ public static class WeekOfExtensions
         /// <returns>The week number in a given year.</returns>
         public int WeekOfYear(CalendarWeekRule calendarWeekRule = CalendarWeekRule.FirstFullWeek)
         {
-            DayOfWeek firstDayOfWeek = CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek;
-            int daysInYear = CultureInfo.CurrentCulture.Calendar.IsLeapYear(date.Year) ? 366 : 365;
-            int weekCount = 0;
-
-            for (int day = 1; day < daysInYear; day++)
-            {
-                DateTime currentDate = new(date.Year, date.Month, day);
-
-                if (day == 1 && calendarWeekRule == CalendarWeekRule.FirstDay)
-                    weekCount = 1;
-                else if (day == 4 && calendarWeekRule == CalendarWeekRule.FirstFourDayWeek)
-                    weekCount = 1;
-                else if (day == 7 && calendarWeekRule == CalendarWeekRule.FirstFullWeek)
-                    weekCount = 1;
-                else
-                {
-                    if (currentDate.DayOfWeek == firstDayOfWeek)
-                        weekCount++;
-                }
-
-                if (currentDate.Day == date.Day)
-                    break;
-            }
-
-            return weekCount;
+            return InternalWeekOfYearCount(date, calendarWeekRule, date.Year, date.Month);
         }
     }
+
+#if NET8_0_OR_GREATER
+    extension(DateOnly date)
+    {
+        /// <summary>
+        /// Calculates the week of the month.
+        /// </summary>
+        /// <returns>The week number in a given month.</returns>
+        public int WeekOfMonth()
+        {
+            return InternalWeekOfMonthCount(DateOnly.ToDateTime(date), date.Year, date.Month);
+        }
+
+        /// <summary>
+        /// Calculates the week in the year of a given DateTime.
+        /// </summary>
+        /// <param name="calendarWeekRule">The rule to use to determine what counts as the 1st week of the year.</param>
+        /// <returns>The week number in a given year.</returns>
+        public int WeekOfYear(CalendarWeekRule calendarWeekRule = CalendarWeekRule.FirstFullWeek)
+        {
+            return InternalWeekOfYearCount(DateOnly.ToDateTime(date),
+                calendarWeekRule, date.Year, date.Month);
+        }
+    }
+#endif
 }
